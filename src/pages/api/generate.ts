04cd6aec233b59521dd3f367424a3cf9d91ef48d
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-15 21:20:31 +0100

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -10,8 +10,6 @@ const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 const sitePassword = import.meta.env.SITE_PASSWORD
 
-import fetch from 'node-fetch';
-
 export const post: APIRoute = async (context) => {
   const body = await context.request.json();
 

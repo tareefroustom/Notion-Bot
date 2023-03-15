81bb183e7e83a67cc5d74e0f04bfae37a2d6c73e
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-15 21:18:11 +0100

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -10,27 +10,37 @@ const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 const sitePassword = import.meta.env.SITE_PASSWORD
 
+import fetch from 'node-fetch';
+
 export const post: APIRoute = async (context) => {
-  const body = await context.request.json()
-  const { sign, time, messages, pass } = body
-  if (!messages) {
-    return new Response('No input text')
-  }
-  if (sitePassword && sitePassword !== pass) {
-    return new Response('Invalid password')
-  }
-  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '', }, sign)) {
-    return new Response('Invalid signature')
-  }
-  const initOptions = generatePayload(apiKey, messages)
-  // #vercel-disable-blocks
-  if (httpsProxy) {
-    initOptions['dispatcher'] = new ProxyAgent(httpsProxy)
-  }
-  // #vercel-end
+  const body = await context.request.json();
 
-  // @ts-ignore
-  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions) as Response
+  const response = await fetch('https://nnq4xy5uj3.execute-api.eu-west-1.amazonaws.com/dev/call', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      User_Email: 'tareef.ramez',
+      sheet_name: 'gpt panda',
+      Excluded_Sheets: ['Tester'],
+      operation: 'Ask_Question',
+      Question: 'List all PD 1L1 drugs alongs with the most negative and most positive sentence about each one of them',
+      Document_URL: 'https://docs.google.com/spreadsheets/d/1rsEva9HsqHjTOr8yAhXzuHH8VK7y4LwtzmX-KIRjovY/edit?usp=sharing',
+      ...body // include any additional data from the original request body
+    })
+  });
 
-  return new Response(parseOpenAIStream(response))
-}
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+
+  const responseData = await response.json();
+
+  return new Response(JSON.stringify(responseData), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+};
+

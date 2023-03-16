87c556f4c041ade39871520af311778276c3651a
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-16 12:00:57 +0100

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -1,3 +1,25 @@
 import { Show } from 'solid-js'
 import type { Accessor, Setter } from 'solid-js'
 import IconEnv from './icons/Env'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+}
+
+export default (props: Props) => {
+  let systemInputRef = "Your name is tareef"
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  return (
+    <div class="my-4">
+    </div>
+  )
+}

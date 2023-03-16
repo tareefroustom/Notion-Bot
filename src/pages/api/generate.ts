506aa4b9a11b3511edd2c8ef955eab07049ea203
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-17 00:25:21 +0100

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -28,7 +28,7 @@ async function concatenateMessages(json) {
 }
 
 async function getCurrentSearchParams() {
-  return new const searchParams = new URLSearchParams(window.location.search);
+  return const searchParams = new URLSearchParams(window.location.search);
 }
 
 export const post: APIRoute = async (context) => {

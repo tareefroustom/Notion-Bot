ee16d48813364924fe9d7e91cd5f0d87e2d6154d
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-16 11:56:09 +0100

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -19,48 +19,5 @@ export default (props: Props) => {
   }
 
   return (
-    <div class="my-4">
-      <Show when={!props.systemRoleEditing()}>
-        <Show when={props.currentSystemRoleSettings()}>
-          <div>
-            <div class="fi gap-1 op-50 dark:op-60">
-              <IconEnv />
-              <span>System Role:</span>
-            </div>
-            <div class="mt-1">
-              { props.currentSystemRoleSettings() }
-            </div>
-          </div>
-        </Show>
-        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
-          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
-            <IconEnv />
-            <span>Add System Role</span>
-          </span>
-        </Show>
-      </Show>
-      <Show when={props.systemRoleEditing() && props.canEdit()}>
-        <div>
-          <div class="fi gap-1 op-50 dark:op-60">
-            <IconEnv />
-            <span>System Role:</span>
-          </div>
-          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
-          <div>
-            <textarea
-              ref={systemInputRef!}
-              placeholder="You are a helpful assistant, answer as concisely as possible...."
-              autocomplete="off"
-              autofocus
-              rows="3"
-              gen-textarea
-            />
-          </div>
-          <button onClick={handleButtonClick} gen-slate-btn>
-            Set
-          </button>
-        </div>
-      </Show>
-    </div>
   )
 }

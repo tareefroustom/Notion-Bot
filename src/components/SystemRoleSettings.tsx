19fd8278f232f9a9deafb1569712c99bad2189f9
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-16 11:59:43 +0100

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -1,11 +1,3 @@
 import { Show } from 'solid-js'
 import type { Accessor, Setter } from 'solid-js'
 import IconEnv from './icons/Env'
-
-interface Props {
-  canEdit: Accessor<boolean>
-  systemRoleEditing: Accessor<boolean>
-  setSystemRoleEditing: Setter<boolean>
-  currentSystemRoleSettings: Accessor<string>
-  setCurrentSystemRoleSettings: Setter<string>
-}

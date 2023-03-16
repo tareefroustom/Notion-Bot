cc32930e0ab7adc2a3880edcfeb87b3745bdedb9
tareefroustom <58273854+tareefroustom@users.noreply.github.com>
2023-03-16 01:48:19 +0100

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -28,15 +28,35 @@ async function concatenateMessages(json) {
 }
 
 
-export const post: APIRoute = async (context) => {
-  const body = await context.request.json();
 
-  return new Response(JSON.stringify(body), {
+export const post: APIRoute = async (context) => {
+  const question = await concatenateMessages(JSON.stringify(context.request.json()));
+  const response = await fetch('https://nnq4xy5uj3.execute-api.eu-west-1.amazonaws.com/dev/call', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      User_Email: 'tareef.ramez',
+      sheet_name: 'gpt panda',
+      Excluded_Sheets: ['Tester'],
+      operation: 'Ask_Question',
+      Question: question,
+      Document_URL: 'https://docs.google.com/spreadsheets/d/1rsEva9HsqHjTOr8yAhXzuHH8VK7y4LwtzmX-KIRjovY/edit?usp=sharing',
+      //...body // include any additional data from the original request body
+    })
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+
+  const responseData = await response.json();
+  return new Response(JSON.stringify(responseData), {
     headers: {
       'Content-Type': 'application/json'
     }
   });
 };
-
 
 
